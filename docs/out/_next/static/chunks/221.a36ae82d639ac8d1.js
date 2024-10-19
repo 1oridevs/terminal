906eb3948e7c08e50756720bea12445fcd92d9a3
Ori Cohen <oricohen@Oris-Mac-Pro.local>
2024-10-19 22:20:51 +0300

    Update basePath for correct CSS loading

---
+++ docs/out/_next/static/chunks/221.a36ae82d639ac8d1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[221],{8221:function(n,t,e){e.r(t),e.d(t,{default:function(){return c},description:function(){return u}});let u="Test command";function c(){return"Pong!"}}}]);
